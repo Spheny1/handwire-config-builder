eb3157b8f3824a1ec1698a88fbe8de17d280628a
Spheny1 <kylenwpb+github@gmail.com>
2023-08-24 16:03:25 -0400

    refactor out config update db seriliaze on keyboard SUCCESSFUL BUILD NOT TESTED

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,9 +1,14 @@
 use rusqlite::{params};
 use tokio_rusqlite::{Connection, Result};
-#[derive(Debug)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Keyboard {
     id: i32,
     name: String,
+    row: String,
+    column: String,
+    orientation: String,
     layout: String,
 //    img: String,
 }
@@ -15,9 +20,12 @@ pub async fn get_keyboard_by_id(conn: &Connection, id: u32) -> Keyboard {
         Ok(stmt.query_row(params![id], |row| {
             return Ok(Keyboard{
                 id: row.get(0).unwrap(),
-                name: row.get(3).unwrap(),
-                layout: row.get(1).unwrap(),
-//                img: row.get(3).unwrap(),
+                name: row.get(1).unwrap(),
+                row: row.get(2).unwrap(),
+                column: row.get(3).unwrap(),
+                orientation: row.get(4).unwrap(),
+                layout: row.get(5).unwrap(),
+//                img: row.get(6).unwrap(),
             })
         }).unwrap())
     }).await.unwrap()
